bddc42fbece630cfe50245baebcbfe0ff419f9a4
wald52 <wald52@protonmail.com>
2025-11-09 17:20:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-// Nom du cache
+// Nom du cache 
 const CACHE_NAME = 'super-site-cache-v1';
 const urlsToCache = [
   '/test-appli-windows/',
@@ -12,30 +12,25 @@ const urlsToCache = [
 // Installation : mise en cache des ressources
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 // Activation : nettoyage des vieux caches
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    })
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      )
+    )
   );
 });
 
 // Interception des requêtes réseau
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
